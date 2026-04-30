bbc3d40db715951b44cdd1e723e39946b9ea5594
NITI Research <dev865077@users.noreply.github.com>
2026-04-29 23:26:46 -0300

    Add Lazy cDLC tree bound proof target

---
+++ spark/src/lazy_cdlc_tree_bound_algebra.adb
@@ -0,0 +1,46 @@
+pragma SPARK_Mode (On);
+
+package body Lazy_Cdlc_Tree_Bound_Algebra is
+   procedure Prove_Geom_Step
+     (B : Branching;
+      N : Step_Layer) is
+   begin
+      null;
+   end Prove_Geom_Step;
+
+   procedure Prove_Lazy_Nodes_Bounded_By_Eager
+     (B : Branching;
+      D : Layer;
+      K : Window_Depth) is
+   begin
+      null;
+   end Prove_Lazy_Nodes_Bounded_By_Eager;
+
+   procedure Prove_Lazy_State_Bounded_By_Eager
+     (B : Branching;
+      D : Layer;
+      K : Window_Depth;
+      P : Weight) is
+   begin
+      null;
+   end Prove_Lazy_State_Bounded_By_Eager;
+
+   procedure Prove_Lazy_State_Independent_Of_Total_Depth
+     (B      : Branching;
+      K      : Window_Depth;
+      P      : Weight;
+      Depth1 : Layer;
+      Depth2 : Layer) is
+   begin
+      null;
+   end Prove_Lazy_State_Independent_Of_Total_Depth;
+
+   procedure Prove_Larger_Window_Weakly_Increases_State
+     (B  : Branching;
+      K1 : Window_Depth;
+      K2 : Window_Depth;
+      P  : Weight) is
+   begin
+      null;
+   end Prove_Larger_Window_Weakly_Increases_State;
+end Lazy_Cdlc_Tree_Bound_Algebra;
